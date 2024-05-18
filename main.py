aa613a2d0c6a78820cb9ede4f17b102cf559a77f
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-05-18 22:44:19 +0900

    add comment

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ def replace_commas(input_file):
     with open(input_file, 'r', encoding='utf-8') as file:
         content = file.read()
     
+    # コンマを全角コンマに置換
     content = content.replace(',', '、')
+
+    # 改行をリセット
     content = "".join(content.splitlines())
+
+    # "(" の後にアラビア数字または漢数字がある場合、その前に改行を追加
     content = re.sub(r"(\()([0-9一二三四五])", r"\n\1\2", content)
 
     return content
