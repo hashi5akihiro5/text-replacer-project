11cfc329da9a75a6d6cbbfb341252d9d4ca791a9
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-06-16 23:42:21 +0900

    [update] "。"の後に改行を追加、ただし")"の場合を除く

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def replace_commas(input_file):
     # コンマを全角コンマに置換
     content = content.replace(",", "、")
 
+    # 数字の後に"口)"がある場合、"()"に置換
+    content = content.replace("口)", "()")
+
     # 全角コンマの後に空文字削除
     content = re.sub(r"、\s", "、", content)
 
@@ -17,15 +20,16 @@ def replace_commas(input_file):
     # "(" の後にアラビア数字または漢数字がある場合、その前に改行を追加
     content = re.sub(r"(\()([0-9一二三四五])", r"\n\1\2", content)
 
-    # アラビア数字の後に"H"ががある場合、その前に改行を追加
+    # 数字の後に"H"ががある場合、その前に改行を追加
     content = re.sub(r"([0-9])H", r"\n\1H", content)
 
-    # "。"の後に改行を追加
-    content = re.sub(r"。", r"。\n", content)
+    # "。"の後に改行を追加、ただし")"の場合を除く
+    # content = re.sub(r"。(?!\))", r"。\n", content)
+    content = re.sub(r"。(?!\))", r"。\n", content)
 
     return content
 
 
 if __name__ == "__main__":
-    input_file = "text_file/202307_航海3級_航海/202307_航海3級_航海.pdf_5.txt"
+    input_file = "text_file/202310_航海1級_航海/202310_航海1級_航海.pdf_5.txt"
     print(replace_commas(input_file))
