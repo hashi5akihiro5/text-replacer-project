1b290b1b62769268caba6443357445f93b3df4a4
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-06-19 12:23:34 +0900

    [update] add def add_space

--- main.py
+++ main.py
@@ -11,12 +11,59 @@ def replace_digits_with_kanji(match):
     return f"({digit_to_kanji(match.group(1))})"
 
 
+# 改行を削除
+def delete_line_break(content):
+    # "また"の前に改行がある場合、その改行を削除
+    content = re.sub(r"\nまた", r"また", content)
+    content = re.sub(r"\n また", r"また", content)
+
+    # "~"の後に改行がある場合、その改行を削除
+    content = re.sub(r"~\n", r"~", content)
+
+    # "の"の後に改行がある場合、その改行を削除
+    content = re.sub(r"の\n", r"の", content)
+
+    # "び"の後に改行がある場合、その改行を削除
+    content = re.sub(r"び\n", r"び", content)
+
+    # "理"の前に改行がある場合、その改行を削除
+    content = re.sub(r"。\n 理", r"。理", content)
+
+    # "計"の前に改行がある場合、その改行を削除
+    content = re.sub(r"。\n計", r"。計", content)
+    content = re.sub(r"。\n 計", r"。計", content)
+
+    # "次"の前に改行がある場合、その改行を削除
+    content = re.sub(r"。\n次", r"。次", content)
+    content = re.sub(r"。\n 次", r"。次", content)
+
+    # "ただし"の前に改行がある場合、その改行を削除
+    content = re.sub(r"。\n ただし", r"。ただし", content)
+    content = re.sub(r"。\nただし", r"。ただし", content)
+
+    return content
+
+
+# スペースを作成
+def add_space(content):
+    # "改行(数字)"の後にスペースを追加
+    content = re.sub(r"(\n\(\d\))(?! )(\D)", r"\1 \2", content)
+
+    # "改行(漢数字)"の後にスペースを追加
+    content = re.sub(r"(\n\([一二三四]\))(?! )(\D)", r"\1 \2", content)
+
+    return content
+
+
 def replace_commas(input_file):
     with open(input_file, "r", encoding="utf-8") as file:
         content = file.read()
 
     # コンマを全角コンマに置換
     content = content.replace(",", "、")
+
+    # "~"を"〜"に置換
+    content = content.replace("~", "〜")
 
     # 数字の後に"口)"がある場合、"()"に置換
     content = content.replace("口)", "()")
@@ -36,15 +83,18 @@ def replace_commas(input_file):
     # 数字の後に"H"ががある場合、その前に改行を追加
     content = re.sub(r"([0-9])H", r"\n\1H", content)
 
-    # "~"の後に改行がある場合、その改行を削除
-    content = re.sub(r"~\n", r"~", content)
-
     # "。"の後に改行を追加、ただし")"の場合を除く
     content = re.sub(r"。(?!\))", r"。\n", content)
+
+    # 改行を削除
+    content = delete_line_break(content)
+
+    # スペースを作成
+    content = add_space(content)
 
     return content
 
 
 if __name__ == "__main__":
-    input_file = "text_file/202402_航海1級_航海/202402_航海1級_航海.pdf_1.txt"
+    input_file = "text_file/202402_航海1級_航海/202402_航海1級_航海.pdf_3.txt"
     print(replace_commas(input_file))
