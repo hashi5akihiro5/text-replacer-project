30b50196dd3dc30184656f4c437fb3a886a420cc
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-07-02 11:51:11 +0900

    [update] replace_arbitrary_characters, add_line_breakの関数を追加

--- main.py
+++ main.py
@@ -18,6 +18,34 @@ def replace_digits_with_kanji(match):
     return f"({digit_to_kanji(match.group(1))})"
 
 
+# 任意の文字を置換
+def replace_arbitrary_characters(content):
+    # コンマを全角コンマに置換
+    content = content.replace(",", "、")
+
+    # "~"を"〜"に置換
+    content = content.replace("~", "〜")
+
+    # 数字の後に"口)"がある場合、"()"に置換
+    content = content.replace("口)", "()")
+
+    return content
+
+
+# 改行を追加
+def add_line_break(content):
+    # "(" の後にアラビア数字または漢数字がある場合、その前に改行を追加
+    content = re.sub(r"(\()([0-9一二三四五])", r"\n\1\2", content)
+
+    # 数字の後に"H"ががある場合、その前に改行を追加
+    content = re.sub(r"([0-9])H", r"\n\1H", content)
+
+    # "。"の後に改行を追加、ただし")"の場合を除く
+    content = re.sub(r"。(?!\))", r"。\n", content)
+
+    return content
+
+
 # 改行を削除
 def delete_line_break(content):
     # "また"の前に改行がある場合、その改行を削除
@@ -83,14 +111,8 @@ def replace_commas(input_file):
     with open(input_file, "r", encoding="utf-8") as file:
         content = file.read()
 
-    # コンマを全角コンマに置換
-    content = content.replace(",", "、")
-
-    # "~"を"〜"に置換
-    content = content.replace("~", "〜")
-
-    # 数字の後に"口)"がある場合、"()"に置換
-    content = content.replace("口)", "()")
+    # 任意の文字を置換
+    content = replace_arbitrary_characters(content)
 
     # 全角コンマの後に空文字削除
     content = re.sub(r"、\s", "、", content)
@@ -98,17 +120,11 @@ def replace_commas(input_file):
     # 改行をリセット
     content = "".join(content.splitlines())
 
-    # "(" の後にアラビア数字または漢数字がある場合、その前に改行を追加
-    content = re.sub(r"(\()([0-9一二三四五])", r"\n\1\2", content)
-
     # '()'内の数字を漢数字置換
     # content = re.sub(r"\((\d)\)", replace_digits_with_kanji, content)
 
-    # 数字の後に"H"ががある場合、その前に改行を追加
-    content = re.sub(r"([0-9])H", r"\n\1H", content)
-
-    # "。"の後に改行を追加、ただし")"の場合を除く
-    content = re.sub(r"。(?!\))", r"。\n", content)
+    # 改行を追加
+    content = add_line_break(content)
 
     # 改行を削除
     content = delete_line_break(content)
